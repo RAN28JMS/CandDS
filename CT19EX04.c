7c9cb1d202e6016afd3260f2b3b848bb19da176e
Black Wizard <109457129+NishanthBlack28@users.noreply.github.com>
2023-08-03 18:20:03 +0530

    Create CT19EX04.c

---
+++ CT19EX04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int power(int x, int y) {
+  int z = 1;
+  for (int i = 0; i < y; i++) {
+    z *= x;
+  }
+  return z;
+}
+
+int main() {
+  int a, b, c;
+  clrscr();
+  printf("Enter a, b: ");
+  scanf("%d%d", &a, &b);
+  c = power(a, b);
+  printf("%d to the power of %d is %d\n", a, b, c);
+  return 0;
+}
+    
