e83f60a0b4ea1e9ce6e99a2bc1f241b40b4af0a2
Black Wizard <109457129+NishanthBlack28@users.noreply.github.com>
2023-08-12 07:54:36 +0530

    Update CT19EX07.c
    12-6-23

--- CT19EX07.c
+++ CT19EX07.c
@@ -5,14 +5,14 @@ void insert(void);
 void delete(void);
 void display(void);
 
-int queue[10], opt,max, rear = -1, front = -1, i, ele, item;
+int queue[10], inc,opt,max, rear = -1, front = -1, i, ele, item;
 
 void main()
 {
   clrscr();
   printf("\n\tEnter the number of elements: ");
   scanf("%d", &max);
-
+  inc=max;
   do
   {
     printf("\n\tSelect an option: ");
@@ -47,7 +47,9 @@ void insert()
   {
     printf("\n\tEnter the element to be inserted: ");
     scanf("%d", &ele);
-    queue[++rear] = ele;
+    rear++;
+    queue[rear] = ele;
+    inc=0;
   }
   else if (rear == max - 1)
   {
@@ -57,7 +59,9 @@ void insert()
   {
     printf("\n\tEnter the element to be inserted: ");
     scanf("%d", &ele);
-    queue[++rear] = ele;
+    rear++;
+    queue[rear] = ele;
+    inc=0;
   }
   printf("\n\tInsertion completed");
 }
@@ -68,15 +72,17 @@ void delete()
   {
     printf("\n\tNo element in queue");
   }
-  else if (front == max)
+  else if (front == max-1)
   {
-    printf("\n\tAll the elements are deleted");
     rear=-1;
+    printf("\n\tAll the elements are deleted");
   }
   else
   {
-    item = queue[++front];
+    front++;
+    item = queue[front];
     queue[front]=0;
+    ++inc;
     printf("\n\tThe deleted element from queue is %d", item);
     
   }
@@ -85,15 +91,17 @@ void delete()
 
 void display()
 {
-  if (rear == -1)
+  if (rear == -1||inc==max)
   {
     printf("\n\tNo element in queue");
   }
   else
   {
     printf("\n\tElements in queue are: ");
-    for (i = 0; i <= rear; ++i)
+    for (i = inc; i <=rear; ++i)
     {
       printf("%d\t", queue[i]);
+    printf("%d",i);
     }
   }
+}
